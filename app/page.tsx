348919b445391977a2ba76b9def08ee4d6abdad6
kishankarthiks <kishankarthiks222@gmail.com>
2026-07-23 14:44:44 +0530

    fix: Update download link to point to new public Lets-Collab-App repository

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Download, MonitorPlay, ShieldCheck, ChevronRight, Zap, Globe, Layers, C
 import { AppLogo } from '@/components/ircp/shared'
 import Link from 'next/link'
 
-const desktopDownloadUrl = "https://github.com/Kish-04/Let-s-Collab-/releases/download/v1.0.0/Let.s.Collab.Setup.0.1.0.exe"
+const desktopDownloadUrl = "https://github.com/Kish-04/Lets-Collab-App/releases/download/v1.0.0/Let.s.Collab.Setup.0.1.0.exe"
 
 // --- Advanced Animation Utilities ---
 
