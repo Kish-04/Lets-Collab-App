9acb173de34db4a4b7bf72a70a61ae57c623ff49
kishankarthiks <kishankarthiks222@gmail.com>
2026-07-23 14:10:47 +0530

    feat: Revert download link back to GitHub releases

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Download, MonitorPlay, ShieldCheck, ChevronRight, Zap, Globe, Layers, C
 import { AppLogo } from '@/components/ircp/shared'
 import Link from 'next/link'
 
-const desktopDownloadUrl = "https://drive.google.com/uc?export=download&id=1DRnMR3jCPmRNqe8ndQKDcwuHKZGeZhog"
+const desktopDownloadUrl = "https://github.com/Kish-04/Let-s-Collab-/releases/download/v1.0.0/Let.s.Collab.Setup.0.1.0.exe"
 
 // --- Advanced Animation Utilities ---
 
