e0622fcd1d7d35f90686461d4bef79a28a679f4a
kishankarthiks <kishankarthiks222@gmail.com>
2026-07-23 13:09:27 +0530

    fix: Update download link for new setup file

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { AppLogo } from '@/components/ircp/shared'
 import Link from 'next/link'
 
 const desktopDownloadUrl = process.env.NEXT_PUBLIC_DESKTOP_DOWNLOAD_URL ||
-  "https://github.com/Kish-04/Let-s-Collab-/releases/download/v1.0.0/Lets-Collab-Cloud-Setup.exe"
+  "https://github.com/Kish-04/Let-s-Collab-/releases/download/v1.0.0/Let.s.Collab.Setup.0.1.0.exe"
 
 // --- Advanced Animation Utilities ---
 
